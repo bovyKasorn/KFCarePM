188b52448d07246e43247895b01b958dca962be3
Bovy Gaysorn <indy.bovy@gmail.com>
2019-02-19 11:05:36 +0700

    npm install

--- android/app/src/main/java/com/kfcarepm/MainApplication.java
+++ android/app/src/main/java/com/kfcarepm/MainApplication.java
@@ -3,6 +3,9 @@ package com.kfcarepm;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.imagepicker.ImagePickerPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -27,6 +30,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImagePickerPackage(),
+            new RNGestureHandlerPackage(),
+            new RNDeviceInfo(),
             new VectorIconsPackage(),
             new ImagePickerPackage(),
             new RNGestureHandlerPackage(),
